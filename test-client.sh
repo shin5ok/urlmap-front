d17f993b0a23afe2d184cc40df6c38bdc3b3c767
Shingo <kawanos@google.com>
2021-08-22 18:05:12 +0900

    modify

--- test-client.sh
+++ test-client.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-HOST=localhost:8081
+HOST=${HOST}
 # register an info
-curl -s localhost:8081/register -H 'Content-Type: application/json' -d '{"org":"https://www.hatena.com","user":"kawanos","redirectPath":"foo","comment":"test"}'
+curl -s ${HOST}/register -H 'Content-Type: application/json' -d '{"org":"https://www.hatena.com","user":"kawanos","redirectPath":"foo","comment":"test"}'
 # infos for user
-curl -s localhost:8081/info/user -H 'Content-Type: application/json'
+curl -s ${HOST}/info/user -H 'Content-Type: application/json'
 # create user
-curl -s localhost:8081/user -H 'Content-Type: application/json' -d '{"user":"tako","notify":"slack#log"}'
+curl -s ${HOST}/user -H 'Content-Type: application/json' -d '{"user":"tako","notify_to":"slack#log"}'
 # get path
-curl -s localhost:8081/get/foo -H 'Content-Type: application/json'
+curl -s ${HOST}/get/foo -H 'Content-Type: application/json'
