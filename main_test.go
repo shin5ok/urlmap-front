87305f70c268d24209ca68b7f4c23923e778a058
Shingo <kawanos@google.com>
2021-11-15 00:56:37 +0900

    create a test

--- main_test.go
+++ main_test.go
@@ -1,25 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/go-playground/assert/v2"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPing(t *testing.T) {
-	w := httptest.NewRecorder()
-	context, _ := gin.CreateTestContext(w)
-	router := gin.Default()
+var testg = CreateRouter()
 
-	req, _ := http.NewRequest("GET", "/Ping", nil)
-	context.Request = req
-	router.GET("/", Ping)
-	// router.ServeHTTP(w, req)
-	t.Run("test /Ping", func(t *testing.T) {
-		assert.Equal(t, http.StatusOK, w.Code)
-	})
+func TestPing(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/Ping", nil)
+
+	testg.ServeHTTP(w, req)
+	log.Println(http.StatusOK)
+	log.Println(w.Code)
+	assert.Equal(t, http.StatusOK, w.Code)
 }
