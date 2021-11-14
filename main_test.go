55ddebaa283bde76623e0adf17d8d976bc7bf1ca
Shingo <kawanos@google.com>
2021-11-15 01:31:15 +0900

    using a TestContext to bind result

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,10 +14,20 @@ var testg = CreateRouter()
 func TestPing(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "/Ping", nil)
+	c, _ := gin.CreateTestContext(w)
+	c.Request, _ = http.NewRequest(http.MethodGet, "/Ping", nil)
 
-	testg.ServeHTTP(w, req)
-	log.Println(http.StatusOK)
-	log.Println(w.Code)
+	testg.ServeHTTP(w, c.Request)
+
 	assert.Equal(t, http.StatusOK, w.Code)
+	// var obj struct {
+	// 	Status  string
+	// 	Version string
+	// 	Message string
+	// }
+	// define struct and create object with it at the same time
+	// c.BindJSON(&obj)
+	// // assert.NoError(t, c.BindJSON(&obj))
+	// log.Printf("%+v", obj)
+	// assert.Equal(t, "Pong", obj.Message)
 }
